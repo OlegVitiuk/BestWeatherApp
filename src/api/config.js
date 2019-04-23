868546b015c55dfbd15fd23e85e401e42b8cb073
Oleg_Valerievich <vitiukoleh@gmail.com>
2019-04-23 04:59:14 +0300

    fix api bug

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5"
+  baseURL: "https://api.openweathermap.org/data/2.5"
 });
 
 const APIKey = "687127fe9b4d07799d1e6e8eeb3d94df";
